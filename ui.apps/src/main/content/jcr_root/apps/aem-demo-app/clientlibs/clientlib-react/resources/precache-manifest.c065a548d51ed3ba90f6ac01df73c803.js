17f743235340583a402f3414a332e6b6a28fe179
ramgarapati <ramgarapati9@gmail.com>
2020-08-20 15:37:35 -0500

    AEM Project

---
+++ ui.apps/src/main/content/jcr_root/apps/aem-demo-app/clientlibs/clientlib-react/resources/precache-manifest.c065a548d51ed3ba90f6ac01df73c803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887bf46eafd14e5dd6aec5d686fe7c75",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "5bddcf5e16829696a6c1",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/static/css/main.e57bbe8a.chunk.css"
+  },
+  {
+    "revision": "6ef205bbabd48f50186d",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/static/js/2.b5a615da.chunk.js"
+  },
+  {
+    "revision": "e7d5bbb3f2be49be916aaab2fc075683",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/static/js/2.b5a615da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bddcf5e16829696a6c1",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/static/js/main.efb2f0fb.chunk.js"
+  },
+  {
+    "revision": "4bae89349ee9a3c7b382",
+    "url": "/etc.clientlibs/aem-demo-app/clientlibs/clientlib-react/resources/static/js/runtime-main.faa44625.js"
+  }
+]);
